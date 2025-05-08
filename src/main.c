47b8b627f25366ecbf640562a33bbeb5b8ee2447
Roberto Del Franco <robertinhodf@gmail.com>
2025-05-08 17:04:27 -0300

    lines

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rdel-fra <rdel-fra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 15:22:45 by rdel-fra          #+#    #+#             */
-/*   Updated: 2025/04/09 16:27:27 by rdel-fra         ###   ########.fr       */
+/*   Updated: 2025/05/08 17:04:00 by rdel-fra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 int	main(void)
 {
 	t_data	*data;
-	char	*input;
 
 	data = (t_data *)ft_calloc(1, sizeof(t_data));
 	while (true)
 	{
-		input = ft_readline();
-		data->prompt = input;
+		data->prompt = ft_readline();
+		if (data->prompt == NULL)
+			return (free(data->prompt), 1);
 		create_token(data);
 	}
 	// function token to receive input
