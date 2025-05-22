443a28400326d6c4027dd9fb55216bd9f615de6c
Roberto Del Franco <robertinhodf@gmail.com>
2025-05-22 14:56:53 -0300

    base structure for executor

---
+++ src/executor/executor.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   executor.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdel-fra <rdel-fra@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/22 14:39:51 by rdel-fra          #+#    #+#             */
+/*   Updated: 2025/05/22 14:51:32 by rdel-fra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+void	*executor(t_data *data)
+{
+	t_node	*cur;
+
+	cur = data->exec_list;
+	while (cur)
+	{
+		if (cur->node_type == PIPE)
+		{
+			printf("Pipe node %d\n", cur->node_type);
+		}
+		else if (cur->node_type == BUILT_IN)
+		{
+			printf("Built-in node %d\n", cur->node_type);
+		}
+		else if (cur->node_type == EXTERNAL)
+		{
+			printf("External node %d\n", cur->node_type);
+		}
+		else
+		{
+			printf("Unknown node type %d\n", cur->node_type);
+		}
+		cur = cur->next;
+	}	
+}
