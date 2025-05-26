ccfd109553158ea6bca05a2ccf2da7e9656f62a7
Roberto Del Franco <robertinhodf@gmail.com>
2025-05-26 09:24:08 -0300

    fixing segfault of env_list

--- lib/libft/ft_strdup.c
+++ lib/libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: rdel-fra <rdel-fra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 10:35:22 by rdel-fra          #+#    #+#             */
-/*   Updated: 2025/03/14 14:56:28 by rdel-fra         ###   ########.fr       */
+/*   Updated: 2025/05/26 09:17:49 by rdel-fra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strdup(const char *s)
 	char	*dest;
 	int		i;
 
+	if (!s)
+		return (NULL);
 	dest = (char *)malloc((ft_strlen((char *)s) + 1) * sizeof(char));
 	if (!dest)
 		return (NULL);
