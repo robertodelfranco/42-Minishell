8ae72bd014c74b6d7cc0b57d654ed123b66de345
Roberto Del Franco <robertinhodf@gmail.com>
2025-05-30 16:40:26 -0300

    correcting list of env with more than one equal sign

--- src/expansion/init_env.c
+++ src/expansion/init_env.c
@@ -6,7 +6,7 @@
 /*   By: rdel-fra <rdel-fra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 17:41:55 by rdel-fra          #+#    #+#             */
-/*   Updated: 2025/05/23 14:29:51 by rdel-fra         ###   ########.fr       */
+/*   Updated: 2025/05/30 16:31:55 by rdel-fra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,8 @@ void	add_env_list(t_data *data, t_env *new_node)
 
 void	ft_init_env(t_data *data, char **env)
 {
-	char	**temp;
+	char	*temp;
+	char	**splited;
 	t_env	*new_node;
 	int		i;
 
@@ -37,16 +38,17 @@ void	ft_init_env(t_data *data, char **env)
 	(void)data;
 	while (env[i])
 	{
-		temp = ft_split(env[i], '=');
+		temp = ft_strchr(env[i], '=');
+		splited = ft_split(env[i], '=');
 		if (temp)
 		{
 			new_node = ft_calloc(1, sizeof(t_env));
 			if (!new_node)
 				return ;
-			new_node->key = ft_strdup(temp[0]);
-			new_node->value = ft_strdup(temp[1]);
+			new_node->key = ft_strdup(splited[0]);
+			new_node->value = ft_strdup(&temp[1]);
 			add_env_list(data, new_node);
-			ft_free_matrix(temp);
+			ft_free_matrix(splited);
 		}
 		i++;
 	}
