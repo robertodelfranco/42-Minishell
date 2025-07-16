cf2acb7a6a758f001b1d311de31e088c45abed05
Roberto Del Franco <robertinhodf@gmail.com>
2025-07-16 14:47:10 -0300

    update exit

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: rdel-fra <rdel-fra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 17:06:06 by rheringe          #+#    #+#             */
-/*   Updated: 2025/07/14 12:18:11 by rdel-fra         ###   ########.fr       */
+/*   Updated: 2025/07/16 14:46:18 by rdel-fra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,23 +37,19 @@ bool	b_exit(t_data *data, char **argv)
 	long	exit_code;
 
 	exit_code = 0;
-	if (argv[1] && argv[2])
+	ft_printf("exit\n");
+	if (argv[1] && !is_numeric(argv[1]))
+	{
+		ft_printf_fd(2, "exit: %s: numeric argument required\n", argv[1]);
+		exit_code = 2;
+	}
+	else if (argv[1] && argv[2])
 	{
 		ft_putendl_fd("exit: too many arguments", 2);
-		data->exit_status = 1;
-		return (false);
+		exit_code = 1;
 	}
-	ft_printf("exit\n");
-	if (argv[1])
-	{
-		if (!is_numeric(argv[1]))
-		{
-			ft_printf_fd(2, "exit: %s: numeric argument required\n", argv[1]);
-			exit_code = 2;
-		}
-		else
-			exit_code = ft_atol(argv[1]) % 256;
-	}
+	else if (argv[1])
+		exit_code = ft_atol(argv[1]) % 256;
 	shutdown_program(data);
 	exit(exit_code);
 }
